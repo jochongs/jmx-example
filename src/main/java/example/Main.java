1b87c3ba3d2deeb820628ba1d904f5f931c8f1b8
jochongs <joch2712@naver.com>
2024-06-30 20:06:12 +0900

    fix: Fix random range

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,23 +1,14 @@
 package example;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 final class RandomApplication {
     private int threadSleepTimeLimit = 3 * 1000;
     private int threadCountLimit = 10;
-
-    // 랜덤값 분포 그래프를 그리기 위한 변수
-    private List<Integer> randomValues = Collections.synchronizedList(new ArrayList<Integer>());
 
     private int incrementValueLimit = 3;
 
@@ -65,7 +56,7 @@ final class RandomApplication {
 
 
     public void printResult() {
-        printResult(3000);
+        printResult(1000);
     }
 
     public void printResult(int milliseconds) {
@@ -81,22 +72,19 @@ final class RandomApplication {
         }
     }
 
-    public synchronized void start() {
+    public void start() {
         // 1초마다 현재 스레드 개수와 value 값을 출력
         new Thread(this::printResult).start();
 
         // 1 ~ n개 사이의 스레드를 랜덤하게 생성하고, join을 통해 전체 종료를 대기 한다
-        int repeatCount = 500;
-        int j = 0;
-        while (j <= repeatCount) {
-            IntStream.range(1, 2).forEach(i -> {
+        while(true) {
+            IntStream.range(1, new Random(System.currentTimeMillis()).nextInt(threadCountLimit)).forEach(i -> {
                 Thread thread = new Thread(() -> {
                     try {
                         // 0 ~ n초 사이의 랜덤한 시간만큼 스레드를 대기
-                        Thread.sleep(1);
+                        Thread.sleep(new Random(System.currentTimeMillis()).nextInt(threadSleepTimeLimit));
                         // -3 ~ 3 사이의 랜덤 정수(증분)
-                        int ra = new Random(System.currentTimeMillis()).nextInt(incrementValueLimit * 2) - incrementValueLimit;
-                        randomValues.add(ra);
+                        int ra = new Random(System.currentTimeMillis()).nextInt(incrementValueLimit * 2 + 1) - incrementValueLimit;
                         value.updateAndGet(v -> v + ra);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -115,43 +103,7 @@ final class RandomApplication {
             });
 
             threads.clear();
-            j++;
         }
-
-        Map<Integer, Integer> integerMap = randomValues.stream()
-                .collect(Collectors.toMap(
-                        value -> value,
-                        value -> 1,
-                        (oldValue, newValue) -> oldValue + 1
-                ));
-
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        for (int i = -3; i <= 3; i++) {
-            Number number = integerMap.get(i);
-            dataset.addValue(number, "Frequency", i);
-        }
-
-        // 차트 생성
-        JFreeChart barChart = ChartFactory.createBarChart(
-                "Value Frequency",
-                "Value",
-                "Frequency",
-                dataset,
-                PlotOrientation.VERTICAL,
-                true, true, false);
-
-        // 차트 패널 생성
-        ChartPanel chartPanel = new ChartPanel(barChart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-
-        // JFrame에 차트 패널 추가
-        JFrame frame = new JFrame();
-        frame.setContentPane(chartPanel);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        System.out.println(integerMap);
     }
 }
 
